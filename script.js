aa7e66fc0218854166411812f514267c1f0d70a8
fatsermint <kyosti.ahti@gmail.com>
2025-12-28 09:26:27 +0200

    Added cute cat

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const copyText = document.getElementById("copyButton")
+const cat = document.getElementById("cat")
+const catP = document.getElementById("pCat")
+const video = document.getElementById("rickroll")
+const div = document.getElementById("shareDiv")
+let mouthOpen = false
 const adress = "https://hsl-data.pages.dev"
 function copyToClipBoard() {
 
@@ -7,3 +12,37 @@ function copyToClipBoard() {
 copyText.addEventListener("click", (e) => {
     copyToClipBoard()
 })
+cat.addEventListener("click", (e) => {
+    catPressed()
+})
+let clicksLeft = 5
+function catPressed() {
+    console.log("meow")
+    clicksLeft--
+    if (clicksLeft > 1) {
+        catP.innerHTML = `Click Me ${clicksLeft} Times`
+    }else{
+        catP.innerHTML = `Just One More`
+
+    }
+    mouthOpen = true
+    cat.src = "meow.png"
+    setTimeout(catNormalize, 500)
+
+    if (clicksLeft < 1) {
+        console.log("super meow")
+        catP.remove()
+        cat.remove()
+        video.play()
+        video.style.display = "block"
+        div.style.display = "block"
+
+    }
+}
+function catNormalize() {
+    cat.src = "cute_cat.png"
+    mouthOpen = false
+}
+video.style.display = "none"
+div.style.display = "none"
+
